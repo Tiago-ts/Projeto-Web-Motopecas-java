3dae1e2405ca43c2a2ae867f0f17064e937c3eba
Tiago-ts <tiagogatewayts@gmail.com>
2020-05-24 10:15:08 -0300

    caixaDAO
    caixaDAO

---
+++ Motopecas(4ºAssessoramento)/Motopecas/src/main/java/br/com/tiago/motopecas/dao/CaixaDAO.java
@@ -0,0 +1,7 @@
+package br.com.tiago.motopecas.dao;
+
+import br.com.tiago.motopecas.domain.Caixa;
+
+public class CaixaDAO extends GenericDAO<Caixa>  {
+
+}
---
+++ Motopecas(4ºAssessoramento)/Motopecas/src/main/java/br/com/tiago/motopecas/dao/MovimentacaoDAO.java
@@ -0,0 +1,7 @@
+package br.com.tiago.motopecas.dao;
+
+import br.com.tiago.motopecas.domain.Movimentacao;
+
+public class MovimentacaoDAO extends GenericDAO<Movimentacao> {
+
+}
